5e19b1d946f8a2e0070939f2affc71d6856130b5
truptigaonkar <trups11.trupti@gmail.com>
2019-09-07 10:37:31 +0200

    udpate

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ecommerce site using Cockpit</h1>
+    </div>
+  );
+}
+
+export default App;
