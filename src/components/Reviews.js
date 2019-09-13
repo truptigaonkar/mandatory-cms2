9ec271cc8001427afe558659aab6ef712ddf8805
truptigaonkar <trups11.trupti@gmail.com>
2019-09-13 14:08:36 +0200

    Create reviews working successfully

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,21 +1,50 @@
 import React, { useState, useEffect } from 'react';
-import Axios from 'axios';
+import axios from 'axios';
 
 const Reviews = (props) => {
     const id = props.id;
     const [reviews, setReviews] = useState([]);
+    const [newReviewsTitle, setNewReviewsTitle] = useState("");
+    const [newReviewsbody, setNewReviewsBody] = useState("");
+    const [newReviewsRating, setNewReviewsRating] = useState("");
 
     // componentDidMount() without hook
     useEffect(() => {
-        Axios.get(`http://192.168.99.102:8085/api/collections/get/reviews/Case?&filter[product._id]=${id}`)
+        axios.get(`http://192.168.99.102:8085/api/collections/get/reviews/Case?&filter[product._id]=${id}`)
             .then(response => {
-                console.log("Reviews entry:", response.data.entries);
+                //console.log("List Reviews entry:", response.data.entries);
                 setReviews(response.data.entries);
             })
     }, [id])
 
+    const handleCreateReview = (e) => {
+        e.preventDefault();
+
+        axios.post(`http://192.168.99.102:8085/api/collections/save/reviews`, {
+            data: {
+                title: newReviewsTitle,
+                body: newReviewsbody,
+                rating: newReviewsRating,
+                product: { _id: id }
+            }
+        })
+            .then(response => {
+                console.log("Create Reviews entry:", response.data);
+                setReviews([...reviews, response.data]);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
+    }
     return (
         <>
+            <div>
+                <h3>Create Reviews</h3>
+                Title: <input type="text" placeholder="Title" onChange={e => setNewReviewsTitle(e.target.value)} value={newReviewsTitle} />
+                Body: <input type="text" placeholder="Body" onChange={e => setNewReviewsBody(e.target.value)} value={newReviewsbody} />
+                Rating: <input type="range" step="1" min="0" max="5" onChange={e => setNewReviewsRating(e.target.value)} value={newReviewsRating} />
+                <button onClick={handleCreateReview}>Submit</button>
+            </div>
             <h3>Reviews page</h3>
             <table border="1">
                 <thead>
@@ -24,7 +53,6 @@ const Reviews = (props) => {
                         <th>Title</th>
                         <th>Body</th>
                         <th>Rating</th>
-                        <th>Product</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -35,12 +63,11 @@ const Reviews = (props) => {
                                 <td>{review.title}</td>
                                 <td>{review.body}</td>
                                 <td>{review.rating}</td>
-                                <td>{review.product.display}</td>
                             </tr>
                         </>
                     ))}
                 </tbody>
-            </table>
+            </table><hr />
         </>
     );
 };
