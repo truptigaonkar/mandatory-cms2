6fd7774340fff4cebc5a4ab16ad498da1a8de2cd
truptigaonkar <trups11.trupti@gmail.com>
2019-09-16 09:48:48 +0200

    Package installed star-rating-tag for showing review rating

--- src/components/Reviews.js
+++ src/components/Reviews.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Button, CardHeader, CardBody, Card, CardText, CardFooter, Input, Badge } from 'reactstrap';
+import { Button, CardHeader, CardBody, Card, CardText, Input, Badge } from 'reactstrap';
 
 const Reviews = (props) => {
     const id = props.id;
@@ -53,27 +53,31 @@ const Reviews = (props) => {
     return (
         <>
             <Card>
-                <CardHeader style={{textAlign:'center'}}><h5>Reviews <Badge color="success">{reviews.length}</Badge></h5></CardHeader>
+                <CardHeader style={{ textAlign: 'center' }}><h5>Reviews <Badge color="success">{reviews.length}</Badge></h5></CardHeader>
                 <CardBody>
                     <p style={{ color: 'green' }}>{successMessage}</p>
                     {reviews.map((review) => (
                         <>
                             <ul key={review._id}>
-                                <b>{review.rating}
+                                <b><star-rating value={review.rating}></star-rating>
                                     {review.title}</b>
                                 <p>{review.body}</p>
                             </ul><hr />
                         </>
                     ))}
                 </CardBody>
-                <Card style={{margin:'20px'}}>
+                <Card style={{ margin: '20px' }}>
                     <CardHeader>Write a review</CardHeader>
                     <CardBody>
                         <CardText>
                             <div style={{ color: 'red' }}>{errorMessage}</div>
                             <Input type="text" placeholder="Title...." onChange={e => setNewReviewsTitle(e.target.value)} value={newReviewsTitle} /><br />
                             <Input type="text" placeholder="Body...." onChange={e => setNewReviewsBody(e.target.value)} value={newReviewsbody} />
-                            <Input type="range" step="1" min="0" max="5" onChange={e => setNewReviewsRating(e.target.value)} value={newReviewsRating} />
+                            {/* <Input type="range" step="1" min="0" max="5" onChange={e => setNewReviewsRating(e.target.value)} value={newReviewsRating} /> */}
+
+                            <Input style={{ width: "100%" }} type="range" name="rating" id="rating" placeholder="Rating" min="0.0" max="5.0" step="0.1" value={newReviewsRating} onChange={e => setNewReviewsRating(e.target.value)} />
+                            <Badge sm={1} color="success">{newReviewsRating}</Badge>  {/* Showing movie rating */}
+
                             <Button color="info" onClick={handleCreateReview}>SEND</Button>
                         </CardText>
                     </CardBody>
