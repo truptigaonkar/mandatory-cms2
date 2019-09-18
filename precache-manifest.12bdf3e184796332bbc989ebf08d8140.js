3313d8a22c41153dc5862fb0cb18c2179e74f0c2
truptigaonkar <trups11.trupti@gmail.com>
2019-09-18 14:44:28 +0200

    Updates

---
+++ precache-manifest.12bdf3e184796332bbc989ebf08d8140.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5023a1e055c7e37fca8fc6388e9ade94",
+    "url": "/mandatory-cms2/index.html"
+  },
+  {
+    "revision": "5b7baabbc9b27f819264",
+    "url": "/mandatory-cms2/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "e580cdeb7313db7c4b3c",
+    "url": "/mandatory-cms2/static/css/main.29eb0fe4.chunk.css"
+  },
+  {
+    "revision": "5b7baabbc9b27f819264",
+    "url": "/mandatory-cms2/static/js/2.bd810a34.chunk.js"
+  },
+  {
+    "revision": "e580cdeb7313db7c4b3c",
+    "url": "/mandatory-cms2/static/js/main.d632cb94.chunk.js"
+  },
+  {
+    "revision": "4094825dc8b4ab32d3e4",
+    "url": "/mandatory-cms2/static/js/runtime~main.f0877a65.js"
+  }
+]);
